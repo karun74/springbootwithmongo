98cf15fc441388feca6a39f46bfad566e048fe06
kshan4 <kshan4@BLRKSHAN469328.sapient.com>
2016-02-02 17:11:17 +0530

    Updated for Dynamic Update of mongo container name through execute script

--- SpringBootWithMongo/src/main/resources/loadData.sh
+++ SpringBootWithMongo/src/main/resources/loadData.sh
@@ -1,5 +1,3 @@
 # $0 is the script name, $1 id the first ARG, $2 is second...
-mongocontainerName="$1"
-fileName="$2"
-docker cp users.json ${mongocontainerName}:/${fileName}
-docker exec -it $mongocontainerName sh -c 'mongoimport --jsonArray --db sample --collection User --type json --file /$fileName'
+docker cp users.json mcName:/var/users.json
+docker exec -it mcNames sh -c 'mongoimport --jsonArray --db sample --collection User --type json --file /var/users.json'
