fec9b33648a80dcbb0432a4e2d6236ec9f5902bf
kshan4 <kshan4@BLRKSHAN469328.sapient.com>
2016-02-02 12:58:47 +0530

    Container name and file name is externalized

--- SpringBootWithMongo/src/main/resources/loadData.sh
+++ SpringBootWithMongo/src/main/resources/loadData.sh
@@ -1,2 +1,5 @@
-docker cp users.json osboxes_mongo_1:/users.json
-docker exec -it osboxes_mongo_1 mongoimport --db sample --collection User --type json --file /users.json --jsonArray
+# $0 is the script name, $1 id the first ARG, $2 is second...
+mongocontainerName="$1"
+fileName="$2"
+docker cp users.json ${mongocontainerName}:/${fileName}
+docker exec -it $mongocontainerName mongoimport --db sample --collection User --type json --file /$fileName --jsonArray
