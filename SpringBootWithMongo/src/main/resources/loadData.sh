ac7e74b90a0cceec5b6ffb2529f550e1d707f5ae
shinjansenin <shinjan.sen@gmail.com>
2016-02-05 02:02:18 +0530

    no message

--- SpringBootWithMongo/src/main/resources/loadData.sh
+++ SpringBootWithMongo/src/main/resources/loadData.sh
@@ -1,3 +1,3 @@
 # $0 is the script name, $1 id the first ARG, $2 is second...
 docker cp users.json mcName:/var/users.json
-docker exec -it mcNames sh -c 'mongoimport --jsonArray --db sample --collection User --type json --file /var/users.json'
+docker exec -it mcName sh -c 'mongoimport --jsonArray --db sample --collection User --type json --file /var/users.json'
